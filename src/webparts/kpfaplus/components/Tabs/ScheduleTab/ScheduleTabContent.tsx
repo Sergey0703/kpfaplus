3c5281e950df25fb8da003195e59c7356587df8a
Baliasnyi Serghii <sergey070373@gmail.com>
2025-05-17 10:56:23 +0100

    222

--- src/webparts/kpfaplus/components/Tabs/ScheduleTab/ScheduleTabContent.tsx
+++ src/webparts/kpfaplus/components/Tabs/ScheduleTab/ScheduleTabContent.tsx
@@ -313,8 +313,8 @@ export const ScheduleTabContent: React.FC<IScheduleTabContentProps> = (props) =>
         <>
           {selectedContract ? (
             <div style={{ 
-              border: '1px solid #e0e0e0', 
-              padding: '15px', 
+                border: 'none',              // Удаляем рамку
+                padding: '0px',    
               borderRadius: '4px',
               minHeight: '300px',
               backgroundColor: 'white'
